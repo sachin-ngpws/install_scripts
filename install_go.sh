10b7c617bff0d470b42a6185ba76af5523d3c5bf
sachin-ngpws <sachin@ngpwebsmart.com>
2020-04-30 10:09:21 +0000

    base commit

---
+++ install_go.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+export GOROOT=/opt
+
+sudo mkdir -p GOROOT
+
+wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+
+sudo tar -C $GOROOT -xzf go1.13.linux-amd64.tar.gz
+
+echo "export PATH=/opt/go/bin:$PATH" | sudo tee -a /etc/environment
+
+echo " export GOPATH=/opt/go" | sudo tee -a /etc/environment
+
+source /etc/environment
